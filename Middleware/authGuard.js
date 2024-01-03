a814d140a2368711c452f9f0fe5922d1799129fe
Rafiul007 <rafiul.faisal@gmail.com>
2024-01-03 22:28:34 +0600

    authGuard middleware added

--- Middleware/authGuard.js
+++ Middleware/authGuard.js
@@ -1,4 +1,15 @@
+const jwt = require("jsonwebtoken");
+const secretKey = "rafiul";
 const authGuard = (req, res, next) => {
-  console.log("Auth guard middleware");
+  const { authorization } = req.headers;
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, secretKey);
+    req.userData = decoded;
+    next();
+  } catch (error) {
+    console.log("Error msg", error);
+    next("Authentication failure");
+  }
 };
 module.exports = authGuard;
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -3,18 +3,19 @@ const bcrypt = require("bcrypt");
 const router = express.Router();
 const User = require("../Model/user.model");
 const jwt = require("jsonwebtoken");
+const authGuard = require("../Middleware/authGuard");
 const secretKey = "rafiul"; // give it .env file letter for protection
 // user signup routes
 router.post("/", async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     User.create({
-    //   fullname: req.body.fullname,
+      //   fullname: req.body.fullname,
       username: req.body.username,
       password: hashedPassword,
       email: req.body.email,
-    //   contactNumber: req.body.contactNumber,
-    //   address: req.body.address,
+      //   contactNumber: req.body.contactNumber,
+      //   address: req.body.address,
     });
     res.status(201).json({
       message: "User created successfully!",
@@ -44,6 +45,8 @@ router.post("/login", async (req, res) => {
       secretKey,
       { expiresIn: "1h" }
     );
+    //659555377672f6fe098eebae
+    //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NTk1NTUzNzc2NzJmNmZlMDk4ZWViYWUiLCJ1c2VyTmFtZSI6ImFkbWluIiwiaWF0IjoxNzA0Mjk4NDcwLCJleHAiOjE3MDQzMDIwNzB9.50UOBxcVLVKCMBcnjBFn1P1xh2PHEnr0PnyhoEinUmA
     res.status(200).json({
       token,
       userId: user._id,
@@ -54,6 +57,31 @@ router.post("/login", async (req, res) => {
     console.log("Error in Login : ", error);
     res.status(400).json({ message: "Server Error" });
   }
+});
+
+//user profile data
+router.get("/profile/:userId", authGuard, async (req, res) => {
+  const userId = req.params.userId;
+  // if (userId !== req.userId) {
+  //   return res.status(403).json({ message: "Forbidden!" });
+  // } else {
+    try {
+      const userData = await User.findById(userId, "-password");
+      if (!userData) {
+        return res.status(404).json({ message: "User not found." });
+      }
+      res.status(200).json(userData);
+    } catch (err) {
+      console.log("Profile Error : ", err);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  // }
+});
+//test
+router.get("/profile", async (req, res) => {
+  res.json({
+    message: "Hello. this is profile"
+  })
 });
 
 module.exports = router;
